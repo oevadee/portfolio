4032784e74f32844615ecff95631503649985193
Adrian Szczechura <a.szczechura.98@gmail.com>
2022-03-01 10:07:54 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
